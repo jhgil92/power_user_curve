825d9bc4f5b3b180c90a69de4d26e64fda49f87b
JIHONG GIL <42768552+jhgil92@users.noreply.github.com>
2020-09-22 12:34:02 +0900

    Update power_user_curve.R
    Add plotly version

--- power_user_curve.R
+++ power_user_curve.R
@@ -23,10 +23,30 @@ query %>%
 data %>% 
   mutate(users = round(count/10000, 1)) %>% 
   ggplot(aes(x = num_active_days, y= users)) +
-  geom_bar(stat="identity", fill='darkgray') +
+  geom_bar(stat = "identity", fill = 'darkgray') +
   scale_x_continuous(breaks = c(5, 10, 15, 20, 25)) +
   ggtitle('The Power User Curve : KIS MTS') +
   ylab('유저수, 만') + xlab('방문일자') +
   theme(plot.title = element_text(hjust = 0.5, size = 15),
         panel.background = element_rect(fill = 'white', colour = alpha('gray', 0.7)),
         panel.grid.major = element_line(color = alpha('gray', 0.3)))
+
+library(plotly)
+data %>% 
+  mutate(
+    users = round(count/10000, 1),
+    rto   = paste0(round(users/sum(users), 2) * 100, '%')
+  ) %>% 
+  plot_ly(
+    x = .$num_active_days,
+    y = .$users,
+    text = paste0(.$users, "(", .$rto, ")"),
+    hoverinfo = 'text',
+    type = 'bar',
+    marker = list(color = alpha('gray', 0.5),
+                  line = list(color = alpha('gray', 0.8),
+                              width = 1.5))
+  ) %>%
+  layout(title = "The Power User Curve : KIS MTS",
+         xaxis = list(title = "방문일자"),
+         yaxis = list(title = "유저수(만)"))
