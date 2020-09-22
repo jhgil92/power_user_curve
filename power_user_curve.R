3678cf206923094579988c5991a10535e4140b51
JIHONG GIL <42768552+jhgil92@users.noreply.github.com>
2020-09-22 09:34:31 +0900

    Create power_user_curve.R

---
+++ power_user_curve.R
@@ -0,0 +1,32 @@
+library(tidyverse)
+library(dplyr)
+library(bigrquery)
+library(ggplot2)
+library(ggthemes)
+
+query <- "
+SELECT num_active_days, COUNT(user_pseudo_id) AS count
+FROM(
+  SELECT user_pseudo_id, count(distinct event_date) as num_active_days
+  FROM `analytics_196410282.events_*` 
+  WHERE _TABLE_SUFFIX BETWEEN FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE('Asia/Seoul'), INTERVAL 28 DAY)) AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE('Asia/Seoul'), INTERVAL 1 DAY))
+  GROUP BY 1
+)
+GROUP BY 1
+ORDER BY 1
+"
+
+query %>%
+  bq_project_query('neo-smart-gcm', .) %>%
+  bq_table_download(start_index = 0) -> data
+
+data %>% 
+  mutate(users = round(count/10000, 1)) %>% 
+  ggplot(aes(x = num_active_days, y= users)) +
+  geom_bar(stat="identity", fill='darkgray') +
+  scale_x_continuous(breaks = c(5, 10, 15, 20, 25)) +
+  ggtitle('The Power User Curve : KIS MTS') +
+  ylab('유저수, 만') + xlab('방문일자') +
+  theme(plot.title = element_text(hjust = 0.5, size = 15),
+        panel.background = element_rect(fill = 'white', colour = alpha('gray', 0.7)),
+        panel.grid.major = element_line(color = alpha('gray', 0.3)))
